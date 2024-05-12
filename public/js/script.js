c8b0fc86cf942566579456c62fbcec262f449235
hosseinvalikhani <hosseinv.dev@gmail.com>
2024-05-12 15:02:10 +0330

    feat: add modal for delete senario

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,34 @@
 'use strict';
+
+// declare array for student data
 let userData = [];
 
-let deleteBtn = [];
-
-// let stuName = document.querySelector('.name').value;
-// let course = document.querySelector('.course').value;
-// let score = document.querySelector('.score').value;
-// userData.push({ stuName, course, score });
+//select table
 const table = document.querySelector('.myTab');
-const body = document.querySelector('body');
-// console.log(body);
-// console.log(table);
-const saveData = function () {
+
+// select add button
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', saveData);
+const inputEmpty = document.querySelector('.input--Empty--text');
+
+function remDan() {
+  inputEmpty.classList.remove('flex');
+  inputEmpty.classList.add('hidden');
+}
+function saveData() {
+  if (
+    document.querySelector('.name').value === '' ||
+    document.querySelector('.course').value === '' ||
+    document.querySelector('.score').value === ''
+  ) {
+    inputEmpty.classList.remove('hidden');
+    inputEmpty.classList.add('flex');
+
+    return 0;
+  }
+
+  remDan();
+
   let stuName = document.querySelector('.name').value;
   let course = document.querySelector('.course').value;
   let score = document.querySelector('.score').value;
@@ -28,61 +45,15 @@ const saveData = function () {
             
           </div>`;
   createRow();
-  // deleteBtn = [];
-  // let dBtn = document.querySelectorAll(".delBtn");
-  // deleteBtn.push(dBtn);
-  // dBtn.forEach((myBtn, i) =>
-  //   document
-  //     .querySelector(`.del-btn-${i + 1}`)
-  //     .addEventListener("click", function () {
-  //       delRow(`${i}`);
-  //     })
-  // );
-  // console.log(dBtn);
-  // console.log(deleteBtn);
-
-  // console.log("slm", document.getElementById("btn-1"));
-  // const deleteFunc = function () {};
-  // deleteBtn.forEach((btn, i) =>
-  //   btn.addEventListener("click", () => console.log("hi"))
-  // );
-};
-
-// function delRow(index) {
-//   // console.log(userData[index]);
-//   userData.splice(index, 1);
-//   console.log(userData);
-//   document.querySelector(`.del-btn-${index + 1}`);
-//   createTable();
-// }
-
-// const createTable = function () {
-//   // table.innerHTML = "";
-
-//   userData.forEach((s, i) => {
-//     addRow(i);
-//   });
-//   table.insertAdjacentHTML("beforeend", rowText);
-//   let dBtn = document.querySelectorAll(".delBtn");
-//   dBtn.forEach((myBtn, i) =>
-//     document
-//       .querySelector(`.del-btn-${i + 1}`)
-//       .addEventListener("click", function () {
-//         delRow(`${i}`);
-//       })
-//   );
-// };
+}
 
 const createRow = function () {
-  userData.forEach((s, i) => {
+  userData.forEach((data, i) => {
     addRow(i);
   });
-
-  // console.log(rowText);
   table.insertAdjacentHTML('beforeend', rowText);
-
-  // console.log(userData);
 };
+
 let rowText = '';
 rowText += `<div class="myTab flex flex-col gap-2 items-center justify-center px-8">
 <div class="flex items-center px-4 gap-8 h-12 border border-black bg-midGreen">
@@ -107,7 +78,7 @@ const addRow = function (index) {
     index + 1
   }" class="delBtn del-btn-${
     index + 1
-  } w-16 bg-red-700" onclick = "remove(this)">delete</button>
+  } w-16 bg-red-700" onclick = "openDeleteModal(this)">delete</button>
         <button id="${index + 1}" data-btn="${
     index + 1
   }" class="editBtn w-16 bg-blue-300" onclick = "editFunc(this)">edit</button>
@@ -118,37 +89,67 @@ const addRow = function (index) {
   document.querySelector('.name').value = '';
   document.querySelector('.course').value = '';
   document.querySelector('.score').value = '';
-
-  // const btn1 = document.getElementById(".btn-1");
-  // btn1.addEventListener("click", () => {
-  //   console.log("kirkhar");
-  // });
-
-  // const text = 'hirad'
-  // table.insertAdjacentHTML("beforeend", text);
-
-  // console.log(rowText);
-
-  // userData[i].forEach((s)=>console.log(s.score,s.stuName,s.course));
 };
 
-const btn = document.querySelector('.btn');
-btn.addEventListener('click', saveData);
+const modalDlt = document.querySelector('.modal--delete');
+// const modalDltHtml = document.querySelector('.modal--html');
+let idNumber;
+function openDeleteModal(button) {
+  modalDlt.classList.remove('hidden');
+  modalDlt.classList.add('flex');
+  overlay.classList.remove('hidden');
+  overlay.classList.add('block');
+  idNumber = button.id;
+  console.log(button);
+  console.log(idNumber);
+  modalDlt.innerHTML = '';
+  // let modalHtml = `<div class="flex  flex-col justify-center items-center px-4 rounded-md gap-8 h-12 min-h-12 mt-2"
+  //  id="row${idNumber}"
+  // >
+  //   <p class="font-poppins">Are you sure?</P>
+  //   <p class="w-36">${userData[idNumber - 1].stuName}</p>
+  //   <p class="w-36">${userData[idNumber - 1].course}</p>
+  //   <p class="w-36">${userData[idNumber - 1].score}</p>
+  //   <p class="w-36 flex gap-4">
+  //       <button id="${idNumber}"
+  // }" class="delBtn del-btn-${idNumber} w-16 bg-red-700" onclick = "remove(this)">delete</button>
+  //       <button id="${idNumber}" data-btn="${idNumber}" class="editBtn w-16 bg-blue-300" onclick = "editFunc(this)">edit</button>
+  //   </p>
 
-function remove(button) {
-  let number = button.id;
-  // let row = document.getElementById(`row${number}`);
-  // row.remove();
+  //   </div>`;
+  let modalHtml = `<button class="close-modal" onclick="cancelDlt()">&times;</button>
+  <div
+    class="flex flex-col justify-center items-center px-4 rounded-md gap-8 mt-2"
+  >
+    <p class="font-poppins">Are you sure?</p>
+    <p class="w-36"><span>Name: </span>${userData[idNumber - 1].stuName}</p>
+    <p class="w-36"><span>Course: </span>${userData[idNumber - 1].course}</p>
+    <p class="w-36"><span>Score: </span>${userData[idNumber - 1].score}</p>
+    <p class="w-36 flex gap-4">
+      <button
+        class="delBtn del-btn-${idNumber} w-16 bgRed"
+        onclick="cancelDlt()"
+      >
+        Cancel
+      </button>
+      <button class="editBtn w-16 backColor2" onclick="remove()">
+        Yes
+      </button>
+    </p>
+  </div>`;
+  console.log(modalHtml);
+  modalDlt.insertAdjacentHTML('beforeend', modalHtml);
+}
+function remove() {
   let count = userData.length;
-  const part1 = userData.slice(0, number - 1);
+  const part1 = userData.slice(0, idNumber - 1);
   console.log(part1);
-  const part2 = userData.slice(number, count);
+  const part2 = userData.slice(idNumber, count);
   console.log(part2);
   userData = part1.concat(part2);
   console.log(userData);
 
   table.innerHTML = '';
-  // console.log(table);
   rowText = `
     <div class="test flex items-center px-4 gap-8 h-12 border border-black bg-midGreen">
             <p class="w-36">Student Number</p>
@@ -159,22 +160,37 @@ function remove(button) {
 
           </div>`;
   createRow();
-}
+  modalDlt.classList.remove('flex');
+  modalDlt.classList.add('hidden');
 
+  overlay.classList.remove('block');
+  overlay.classList.add('hidden');
+}
+function cancelDlt() {
+  console.log(modalDlt);
+  modalDlt.classList.remove('flex');
+  modalDlt.classList.add('hidden');
+
+  overlay.classList.remove('block');
+  overlay.classList.add('hidden');
+}
+// edit modal
 let number;
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 
 function editFunc(button) {
+  number = button.id;
   modal.classList.remove('hidden');
   modal.classList.add('block');
   overlay.classList.remove('hidden');
   overlay.classList.add('block');
-
-  number = button.id;
+  console.log(userData[number - 1].course);
+  document.querySelector('.nameModal').value = userData[number - 1].stuName;
+  document.querySelector('.courseModal').value = userData[number - 1].course;
+  document.querySelector('.scoreModal').value = userData[number - 1].score;
 }
 function done() {
-  // const modal = document.querySelector('.modal');
   let count = userData.length;
   let stuName = document.querySelector('.nameModal').value;
   let course = document.querySelector('.courseModal').value;
@@ -211,7 +227,6 @@ function done() {
 }
 
 function cancel() {
-  // const modal = document.querySelector('.modal');
   let stuName = document.querySelector('.nameModal').value;
   let course = document.querySelector('.courseModal').value;
   let score = document.querySelector('.scoreModal').value;
@@ -226,63 +241,5 @@ function cancel() {
   overlay.classList.remove('block');
   overlay.classList.add('hidden');
 }
-// console.log("slm", document.getElementById("btn-1"));
 
-// const deleteRow = function(e){
-//     console.log(e.target);
-//     rowText = '';
-//     createRow();
-
-// }
-// const delBtn = document.querySelectorAll('.delBtn');
-// delBtn.addEventListener('click', () => console.log('delete clicked'));
-
-// const remove = function(){
-//     userData.shift();
-//     rowText = '';
-//     console.log(userData);
-//     createRow();
-// }
-// const rembtn = document.querySelector('.rem');
-// rembtn.addEventListener('click', remove);
-
-// document.addEventListener('DOMContentLoaded', (event) => {
-//   // Function to handle the click event on the 'add' button
-//   document.querySelector('.bg-midGreen').addEventListener('click', () => {
-//     // Get the input values
-
-//     // Add the values to the array
-//     userData.push({ name, course, score });
-
-//     // Update the table with the new data
-//     updateTable();
-//   });
-// });
-
-// Function to create/update the table with user data
-// function updateTable() {
-//   // Create a table if it doesn't exist
-//   let table = document.querySelector('table') || document.createElement('table');
-//   table.innerHTML = ''; // Clear the table
-
-//   // Create the header row
-//   let headerRow = table.insertRow();
-//   let headers = ['Name', 'Course', 'Score'];
-//   headers.forEach(headerText => {
-//     let header = document.createElement('th');
-//     header.textContent = headerText;
-//     headerRow.appendChild(header);
-//   });
-
-//   // Add data to the table
-//   userData.forEach(item => {
-//     let row = table.insertRow();
-//     Object.values(item).forEach(text => {
-//       let cell = row.insertCell();
-//       cell.textContent = text;
-//     });
-//   });
-
-//   // Append the table to the body or update the existing table
-//   document.body.appendChild(table);
-// }
+// delete modal
